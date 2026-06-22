565da5f31b01eadaf80160cbc176d617e30037ea
Prathamesh_Chaudhari <prathamesh.chaudhari@primaverse.com>
2026-06-22 18:16:12 +0530

    add one more tool

--- arithmathic-agent.py
+++ arithmathic-agent.py
@@ -11,6 +11,10 @@ def multiply(x, y):
     """Useful for multiplying two numbers"""
     return x * y
 
+def add(x, y):
+    """Useful for adding two numbers"""
+    return x + y
+
 # Use Google SDK class
 llm = GoogleGenAI(
     model="gemini-2.5-flash",
@@ -21,13 +25,13 @@ llm = GoogleGenAI(
 agent = FunctionAgent(
     name="Calc",
     description="Multiply two numbers",
-    tools=[multiply],
+    tools=[multiply, add],
     llm=llm,
 )
 
 # Standard asyncio wrapper to handle the workflow properly
 async def main():
-    response = await agent.run("What is 1234 * 4567?")
+    response = await agent.run("What is 1234 + 4567?")
     print(f"Agent response: {response}")
 
 if __name__ == "__main__":
